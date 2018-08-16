b2e2d0a5dfc2393967dd8be84ad27e27620a4a2c
Daniel Miller <danm1113@gmail.com>
2018-08-16 14:35:47 -0700

    Arguements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[macro_use]
+extern crate structopt;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "tomat", about = "A simple, cli pomodoro timer")]
+enum Timers {
+    /// Timer Type
+    #[structopt(name = "timer")]
+    Timer {
+        #[structopt(parse(from_str))]
+        timer_type: String
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Timers::from_args();
+    println!("Args; {:?}", args);
 }

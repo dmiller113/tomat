115ea83522732eb2fe2a953435c63294c22023cb
Daniel Miller <danm1113@gmail.com>
2019-12-16 13:13:32 -0800

    Added in v0.5 functionality. We blocking now.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::time::{ Duration };
+use std::thread::sleep;
+
 #[macro_use]
 extern crate structopt;
 
 use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
+#[derive(Clone, Debug, StructOpt)]
 #[structopt(name = "tomat", about = "A simple, cli pomodoro timer")]
 struct Opt {
     // is this a break
@@ -11,10 +14,32 @@ struct Opt {
     /// This pomodoro is a long break
     is_break: bool,
 
+    #[structopt(short = "d", long = "debug")]
+    is_debug: bool,
+
     pomodoro_name: String,
 }
 
 fn main() {
     let args = Opt::from_args();
-    println!("Args; {:?}", args);
+    let Opt { pomodoro_name, is_break, is_debug } = args.clone();
+
+    if is_debug {
+       println!("Args: {:?}", args); 
+    }
+
+    let duration_in_minutes = if is_break {
+        15
+    } else {
+        25
+    };
+
+    println!(
+        "Starting pomodoro {}! See you in {} minutes.",
+        pomodoro_name,
+        duration_in_minutes
+    );
+
+    sleep(Duration::from_secs(duration_in_minutes * 60));
+    println!("ring ring ring! {} is done!", pomodoro_name);
 }

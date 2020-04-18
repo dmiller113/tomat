533dc6400d1248bd62558fa9358c77263bfdd147
Daniel Miller <danm1113@gmail.com>
2020-04-17 18:24:12 -0700

    Move into a structure that more closely resembles the final struct.

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,59 @@ extern crate structopt;
 
 use structopt::StructOpt;
 
-#[derive(Clone, Debug, StructOpt)]
+#[derive(Clone, StructOpt)]
 #[structopt(name = "tomat", about = "A simple, cli pomodoro timer")]
 struct Opt {
     #[structopt(short = "d", long = "debug")]
     /// Show debug information
     is_debug: bool,
 
+    #[structopt(subcommand)]
+    cmd: Option<Command>,
+
+    #[structopt(name = "task name")]
     /// Name of pomodoro.
-    pomodoro_name: String,
+    task_name: Option<String>,
+}
+
+#[derive(Clone, StructOpt)]
+enum Command {
+    /// Add to the list of active pomodoro
+    #[structopt(name = "add")]
+    Add {
+        #[structopt(name = "task name")]
+        /// Name of pomodoro.
+        task_name: String,
+    },
+
+    /// Show list of active pomodoro
+    #[structopt(name = "list")]
+    List {
+        #[structopt(short = "a", long = "all")]
+        /// Show cancelled and finished pomodoros
+        show_ended_tasks: bool,
+    }
 }
 
 fn main() {
     let args = Opt::from_args();
-    let Opt { pomodoro_name, is_debug } = args.clone();
-
-    if is_debug {
-       println!("Args: {:#?}", args); 
-    }
+    let Opt {
+        is_debug: _,
+        cmd: _,
+        task_name,
+    } = args.clone();
 
     let duration_in_minutes = 25;
 
-    println!(
-        "Starting pomodoro {}! See you in {} minutes.",
-        pomodoro_name,
-        duration_in_minutes
-    );
+    task_name.map(|name| {
+        println!(
+            "Starting pomodoro {}! See you in {} minutes.",
+            name,
+            duration_in_minutes
+        );
 
-    sleep(Duration::from_secs(duration_in_minutes * 60));
-    println!("ring ring ring! {} is done!", pomodoro_name);
+        sleep(Duration::from_secs(duration_in_minutes * 60));
+        println!("ring ring ring! {} is done!", name);
+        name
+    });
 }

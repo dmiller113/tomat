641fc105a8772458979053a8551b2795e3f51c5e
Daniel Miller <danm1113@gmail.com>
2020-04-24 17:09:51 -0700

    Start work on start command view.

--- src/start.rs
+++ src/start.rs
@@ -1,6 +1,34 @@
 use crate::config::Config;
 use crate::view::Console;
+use crate::types::Pomodoro;
 use std::format;
+use std::time::Instant;
+
+
+struct StartView {
+    // List of upcoming pomodoros
+    upcoming: Vec<Pomodoro>,
+
+    // Current pomodoro
+    current: Pomodoro,
+
+    // Time this view was started at
+    created_at: Instant,
+}
+
+impl StartView {
+    pub fn new(pomodoros: Vec<Pomodoro>) -> Self {
+        let (current, upcoming) = match pomodoros.split_first() {
+            Some((first, rest)) => (first.to_owned(), rest.to_vec()),
+            None => panic!("Oops")
+        };
+        StartView {
+            created_at: Instant::now(),
+            current,
+            upcoming,
+        }
+    }
+}
 
 pub fn start_pomodoro(duration: u32, task_name: String, config: Config) {
     let mut console = Console::setup(config)
